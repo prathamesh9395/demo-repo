2ff1608268803d972203a4802d7b50963a6c66fe
siddhesh gurav <prathmesh9395@gmail.com>
2024-08-14 21:02:20 +0530

    wrote a code of two sum

--- index.py
+++ index.py
@@ -1,2 +1,12 @@
 print("hello world...!")
 print("hello world...!")
+def fun(l1, tar):
+    d={}
+    for i,num in enumerate(l1):
+        d[num] = i
+    for i in d:
+        c = tar - i
+        if c in d and d[c]!= 1:
+            return d[i] , d[c]
+l=[6,5,7,8,9,3]
+print(fun(l, 10))
